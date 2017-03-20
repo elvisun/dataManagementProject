5312ae1d7be2db83853e8bf1668cf76869b5d135
Elvis Sun <elvissunlihe@gmail.com>
2017-03-20 11:39:47 -0400

    generate data

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbHostname     = 'databaseforproject.cjffpyni9e6r.us-east-2.rds.amazonaws.com';
+$dbName         = 'sys';
+$dbUsername     = 'databaseuser';
+$dbPassword     = 'databasepassword';
+$dbTablePrefix  = 'gd_1';
+$encryptionSalt = 'pIv';
